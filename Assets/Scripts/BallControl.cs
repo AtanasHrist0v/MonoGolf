2d5586c9800b0553cec1aa448f422767c9d7a4a6
levaka5459 <levaka5459@mail.bg>
2018-10-27 16:47:08 +0300

    Added shot counter and reworked the aiming mechanic
    Aiming now checks the difference between current finger position and ball (instead of first finger position and current).
    Camera is now centered around the ball's x AND y axises.
    Releasing your finger on top of the ball now cancels your shot.

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class BallControl : MonoBehaviour {
 
     public GameObject cam, pivot, plane;
+    public Text counter;
+    public LevelBuilder levelBuilder;
     float angle, drawLength, speedMultiplier = 20F;
     Vector3 startPos, endPos, direction, force, lastPos;
     bool shot = false, mouseHeld = false;
     Quaternion rotation;
+    
     // Use this for initialization
     void Start () {
         Input.simulateMouseWithTouches = true;
@@ -22,8 +24,9 @@ public class BallControl : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0) && GetComponent<Rigidbody>().velocity == Vector3.zero)
         {
-            startPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            startPos = new Vector3(cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).x, cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).y, 0);
             plane.GetComponent<MeshRenderer>().enabled = true;
+            
         }
 
         if (Input.GetMouseButton(0) && GetComponent<Rigidbody>().velocity == Vector3.zero)
@@ -76,14 +79,18 @@ public class BallControl : MonoBehaviour {
             pivot.transform.position = new Vector3(GetComponent<Transform>().position.x, 0.52F, GetComponent<Transform>().position.z);
             pivot.transform.rotation = rotation;
             mouseHeld = true;
+            if (endPos.x >= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).x - 50 && endPos.x <= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).x + 50 && endPos.y >= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).y - 50 && endPos.y <= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).y + 50) plane.GetComponent<MeshRenderer>().enabled = false;
         }
         if (mouseHeld && Input.GetMouseButton(0) == false)
         {
-            lastPos = GetComponent<Transform>().position;
+            if (!(endPos.x >= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).x - 50 && endPos.x <= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).x + 50 && endPos.y >= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).y - 50 && endPos.y <= cam.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position).y + 50)) {
+                GetComponent<Rigidbody>().AddRelativeForce(force);
+                lastPos = GetComponent<Transform>().position;
+                shot = true;
+                levelBuilder.shotsLeft--;
+            }
             plane.GetComponent<MeshRenderer>().enabled = false;
-            GetComponent<Rigidbody>().AddRelativeForce(force);
-
-            shot = true;
+            plane.GetComponent<MeshRenderer>().enabled = false;
             mouseHeld = false;
         }
 
@@ -95,12 +102,12 @@ public class BallControl : MonoBehaviour {
             GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
 
-
+        counter.text = "Shots left: " + levelBuilder.shotsLeft;
     }
 
     void LateUpdate()
     {
-        cam.transform.position = new Vector3(0, 12, GetComponent<Transform>().position.z + (float)cam.GetComponent<Camera>().pixelWidth / 350);
+        cam.transform.position = new Vector3(GetComponent<Transform>().position.x, 12, GetComponent<Transform>().position.z + (float)cam.GetComponent<Camera>().pixelWidth / 350);
         if (cam.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(0, 0, cam.GetComponent<Camera>().nearClipPlane)).z >= 40) cam.transform.position = new Vector3(0, 12, 40.2F);
 
         if (GetComponent<Rigidbody>().velocity.z == 0)
--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour {
@@ -7,17 +6,25 @@ public class LevelBuilder : MonoBehaviour {
     // Use this for initialization
     public List<GameObject> prefabList = new List<GameObject>();
     float Pos = 7F;
+    public byte shotsLeft;
+    int randNumber;
     void Start () {
         
         for (int i = 0; i < 2; i++)
         {
-            Instantiate(prefabList[Random.Range(0, 2)], new Vector3(2.5F, 0.5F, Pos + 15 * i), Quaternion.identity);
+            randNumber = Random.Range(0, 2);
+            Instantiate(prefabList[randNumber], new Vector3(2.5F, 0.5F, Pos + 15 * i), Quaternion.identity);
+            switch (randNumber)
+            {
+                case 0: shotsLeft += 3;
+                    break;
+                case 1: shotsLeft += 2;
+                    break;
+            }
+            shotsLeft += 2;
         }
         Instantiate(prefabList[2], new Vector3(2.5F, 0.5F, Pos + 15 * 2), Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update() { }
 }
