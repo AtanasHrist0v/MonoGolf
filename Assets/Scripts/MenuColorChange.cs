abbd0de96dbb661969a61603e69514522b8ffdd6
DrHristov <the_doktora@abv.bg>
2018-12-20 15:05:12 +0200

    UI Improvements

--- Assets/Scripts/MenuColorChange.cs
+++ Assets/Scripts/MenuColorChange.cs
@@ -6,45 +6,55 @@ using UnityEngine.UI;
 public class MenuColorChange : MonoBehaviour 
 {
 	public Camera cam;
-
-	void Start () 
+	public Button button1, button2, button3;
+	void Start ()
 	{
-		int i = Random.Range (1, 7);
+		int i = 2;//Random.Range (1, 7);
 		Debug.Log ("i = " + i);
-
-		Color c1 = new Color32 (63,85,173, 0);
-		Color c2 = new Color32 (63,173,110, 0);
-		Color c3 = new Color32 (63,173,63, 0);
-		Color c4 = new Color32 (191,188,28, 0);
-		Color c5 = new Color32 (200,54,45, 0);
-		Color c6 = new Color32 (231,95,116, 0);
-		Color c7 = new Color32 (170,44,190, 0);
-
 		switch (i)
 		{
 		case 1:
-			cam.backgroundColor = c1;
+			cam.backgroundColor = new Color32 (63,85,173, 0);
+			button1.image.color = new Color32(6,115,255,255);
+			button2.image.color = new Color32(6,115,255,255);
+			button3.image.color = new Color32(6,115,255,255);
 			break;
 		case 2:
-			cam.backgroundColor = c2;
-			//Canvas.FindObjectOfType<Button>()>().image = Color.black;
+			cam.backgroundColor = new Color32 (63, 173, 110, 0);
+			button1.image.color = new Color32();
+			button2.image.color = new Color32();
+			button3.image.color = new Color32();
 			break;
 		case 3:
-			cam.backgroundColor = c3;
+			cam.backgroundColor = new Color32 (63,173,63, 0);
+			button1.image.color = new Color32();
+			button2.image.color = new Color32();
+			button3.image.color = new Color32();
 			break;
 		case 4:
-			cam.backgroundColor = c4;
+			cam.backgroundColor = new Color32 (191,188,28, 0);
+			button1.image.color = new Color32();
+			button2.image.color = new Color32();
+			button3.image.color = new Color32();
 			break;
 		case 5:
-			cam.backgroundColor = c5;
+			cam.backgroundColor = new Color32 (200,54,45, 0);
+			button1.image.color = new Color32();
+			button2.image.color = new Color32();
+			button3.image.color = new Color32();
 			break;
 		case 6:
-			cam.backgroundColor = c6;
+			cam.backgroundColor = new Color32 (231,95,116, 0);
+			button1.image.color = new Color32();
+			button2.image.color = new Color32();
+			button3.image.color = new Color32();
 			break;
 		case 7:
-			cam.backgroundColor = c7;
+			cam.backgroundColor = new Color32 (170,44,190, 0);
+			button1.image.color = new Color32();
+			button2.image.color = new Color32();
+			button3.image.color = new Color32();
 			break;
 		}
 	}
-
 }
